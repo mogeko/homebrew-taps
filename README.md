d8bffb8a48c10cb6f25e55aac6592576adfd6a50
Mogeko <zhengjunyi@live.com>
2019-02-10 13:23:50 +0800

    Update a new branch to auto-update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# homebrew-taps
-Mogeko's wine cellar.
+# auto-update
+
+This branch is used to automatically update [`homebrew-taps`](https://github.com/Mogeko/homebrew-taps).
---
+++ update.repo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+update_repo(){
+    cd homebrew-taps
+    status=`git status | grep "working tree clean" &> /dev/null; echo "$?"`
+    if [ "$status" == "1" ]; then
+        git commit -am "travis automated update" || exit 0
+        git push  --quiet "https://${GITHUB_TOKEN}@${GH_REF}" master:master
+    fi
+    cd ..
+}
+
+
+[ "$0" = "update.repo.sh" ] && git clone https://${GH_REF}
+[ "$0" = "update.repo.sh" ] && update_repo
+
