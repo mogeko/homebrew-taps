f702172e622e57e056b1db77c98849fee57f194c
Mogeko <zhengjunyi@live.com>
2019-02-12 20:12:52 +0800

    update: add man-page for vlmcsd

--- Formula/vlmcsd.rb
+++ Formula/vlmcsd.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Vlmcsd < Formula
   desc "KMS Emulator in C (currently runs on Linux including Android, FreeBSD, Solaris, Minix, Mac OS, iOS, Windows with or without Cygwin)"
   homepage "http://forums.mydigitallife.info/threads/50234"
@@ -16,6 +13,12 @@ class Vlmcsd < Formula
     system "make", "CC=gcc"
     bin.install "bin/vlmcsd"
     bin.install "bin/vlmcs"
+    man1.install "man/vlmcs.1"
+    man7.install "man/vlmcsd.7"
+    man8.install "man/vlmcsd.8"
+    man5.install "man/vlmcsd.ini.5"
+    # man7.install "man/vlmcsd-floppy.7"
+    # man1.install "man/vlmcsdmulti.1"
   end
 
   test do
