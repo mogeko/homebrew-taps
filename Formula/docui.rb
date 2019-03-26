22aebab3e7f39102e5ddecfa6a8abb27463ea542
Mogeko <zhengjunyi@live.com>
2019-03-26 10:45:30 +0800

    fix: Update dependency information for docui

--- Formula/docui.rb
+++ Formula/docui.rb
@@ -6,7 +6,7 @@ class Docui < Formula
   sha256 "044ce6c54cf8b8bae6165942c8ff87d51594abc246863f00b5201e8cc538af79"
 
   depends_on "go" => :build
-  depends_on "docker"
+  # depends_on "docker"
 
   def install
     # ENV["GO111MODULE"] = "on"
@@ -14,6 +14,13 @@ class Docui < Formula
     system "make"
 
     bin.install "docui"
+  end
+
+  def caveats; <<~EOS
+    Please make sure Docker is installed on your computer.
+    If NOT, you can install it by
+        brew cask install docker
+  EOS
   end
 
   test do
