3b4614a28a13f79a46d49744753a9dc5bf71f9fd
Mogeko <zhengjunyi@live.com>
2019-03-26 13:00:10 +0800

    update: add kcptun

---
+++ Formula/kcptun.rb
@@ -0,0 +1,28 @@
+class Kcptun < Formula
+  desc "A Stable & Secure Tunnel Based On KCP with N:M Multiplexing"
+  homepage "https://github.com/xtaci/kcptun"
+  version "v20190325"
+  url "https://github.com/xtaci/kcptun/archive/#{version}.tar.gz"
+  sha256 "1bbceaaf3766de43eeddd47b779a5bcb913ff4192a560f0bcad6cccf6cd7ec3c"
+
+  depends_on "go" => :build
+
+  def install
+    # ENV["GO111MODULE"] = "on"
+    ENV["GOPROXY"] = "https://goproxy.io"
+    system "go", "build", "-o", "bin/kcptunc", "./client"
+    system "go", "build", "-o", "bin/kcptuns", "./server"
+
+    bin.install "bin/kcptunc"
+    bin.install "bin/kcptuns"
+  end
+
+  test do
+    client_version = shell_output("#{bin}/kcptunc -v")
+    assert_match "kcptun version SELFBUILD", client_version
+    server_version = shell_output("#{bin}/kcptuns -v")
+    assert_match "kcptun version SELFBUILD", server_version
+    system "kcptunc", "-v"
+    system "kcptuns", "-v"
+  end
+end
