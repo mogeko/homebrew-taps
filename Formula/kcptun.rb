b5ed1f22cdd06e9c391ee530f31f92551cd482f8
Travis <zhengjunyi@live.com>
2019-04-02 07:31:48 +0000

    travis automated update

--- Formula/kcptun.rb
+++ Formula/kcptun.rb
@@ -1,9 +1,9 @@
 class Kcptun < Formula
   desc "A Stable & Secure Tunnel Based On KCP with N:M Multiplexing"
   homepage "https://github.com/xtaci/kcptun"
-  version "v20190325"
+  version "v20190401"
   url "https://github.com/xtaci/kcptun/archive/#{version}.tar.gz"
-  sha256 "1bbceaaf3766de43eeddd47b779a5bcb913ff4192a560f0bcad6cccf6cd7ec3c"
+  sha256 "70d01d99cc7ae5b77a0943c61377216a0f758d2b2ac3d60786af8f6e9a66065e"
 
   depends_on "go" => :build
 
