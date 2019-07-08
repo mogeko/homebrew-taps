3e11b0a054e0d8869534b0be1c54d9bbcd0e59b1
Travis <zhengjunyi@live.com>
2019-07-08 15:41:49 +0000

    kcptun 20190708

--- Formula/kcptun.rb
+++ Formula/kcptun.rb
@@ -1,9 +1,9 @@
 class Kcptun < Formula
   desc "A Stable & Secure Tunnel Based On KCP with N:M Multiplexing"
   homepage "https://github.com/xtaci/kcptun"
-  version "v20190611"
+  version "20190708"
   url "https://github.com/xtaci/kcptun/archive/#{version}.tar.gz"
-  sha256 "eda1e14137145e0ea3f7af6f7567eea3ca402bff90efc2a87647445871bbbd28"
+  sha256 "602e3c9a9b514ce6a4e94be38430e97395c2ddd0b319059257a39be7e2caec7d"
 
   depends_on "go" => :build
 
