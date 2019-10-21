210540cbbc32cb6ac1e3b69efe4c407bce9a3073
Travis <zhengjunyi@live.com>
2019-10-21 16:33:37 +0000

    stegify v1.1

--- Formula/stegify.rb
+++ Formula/stegify.rb
@@ -1,9 +1,9 @@
 class Stegify < Formula
   desc "Go tool for LSB steganography, capable of hiding any file within an image."
-  version "v1.0"
+  version "v1.1"
   homepage "https://github.com/DimitarPetrov/stegify"
   url "https://github.com/DimitarPetrov/stegify/archive/#{version}.tar.gz"
-  sha256 "0bffdca66c1c6b56a069a468d50a24c66e463faf2e21f63408cadd7f0215db3f"
+  sha256 "33c4111447a0e2538e211c1b787b726e895b57a2f25a4f7a11002e1f3357d1f4"
 
   depends_on "go" => :build
 
