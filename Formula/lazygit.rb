b689c3dd5dd8ef023856e5155c19778ca6c048f7
Travis <zhengjunyi@live.com>
2020-01-10 17:08:45 +0000

    lazygit v0.13

--- Formula/lazygit.rb
+++ Formula/lazygit.rb
@@ -1,9 +1,9 @@
 class Lazygit < Formula
   desc "simple terminal UI for git commands"
   homepage "https://github.com/jesseduffield/lazygit"
-  version "v0.12.3"
+  version "v0.13"
   url "https://github.com/jesseduffield/lazygit/archive/#{version}.tar.gz"
-  sha256 "0338c29be43ab1393ed9dfcda20a93c44f40753c26c8e516b6ed7f79f7275ec4"
+  sha256 "93ca0847cd91874228b023d9feb967aaa819532f173fd6e19e2d00b8a6242e3c"
 
   depends_on "go" => :build
   depends_on "git"
