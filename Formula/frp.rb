b5c0b3de35b17e6833f8b964ce1acf097f439b08
Travis <zhengjunyi@live.com>
2020-03-11 17:41:01 +0000

    frp v0.32.0

--- Formula/frp.rb
+++ Formula/frp.rb
@@ -1,9 +1,9 @@
 class Frp < Formula
   desc "A fast reverse proxy to help you expose a local server behind a NAT or firewall to the internet."
   homepage "https://github.com/fatedier/frp"
-  version "v0.31.2"
+  version "v0.32.0"
   url "https://github.com/fatedier/frp/archive/#{version}.tar.gz"
-  sha256 "790a18f5651cc645a3d73fc147c719f5b212fc21db0c51c620011ee836b7a28f"
+  sha256 "39162780b28c0019207d83919530b573fac0bef8df30f1b6a5860886b0616c67"
 
   depends_on "go" => :build
 
