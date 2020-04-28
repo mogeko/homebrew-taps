2ec05680217df878f169c100096db11259775d1e
Travis <zhengjunyi@live.com>
2020-04-28 18:04:54 +0000

    gameboy.live 0.1

--- Formula/gameboy.live.rb
+++ Formula/gameboy.live.rb
@@ -3,9 +3,9 @@
 class GameboyLive < Formula
   desc 'A basic gameboy emulator with terminal "Cloud Gaming" support'
   homepage 'https://github.com/HFO4/gameboy.live'
-  version '0.1'
+  version "0.1"
   url "https://github.com/HFO4/gameboy.live/archive/#{version}.tar.gz"
-  sha256 '2923467649b188ebd02c8994fa5e49a2264f0a27fdbb56539140ae521d96b2fc'
+  sha256 "2923467649b188ebd02c8994fa5e49a2264f0a27fdbb56539140ae521d96b2fc"
 
   depends_on 'go' => :build
 
