039d04b9e50089f7a066ce0ebc49fbd2a46ba898
Mogeko <zhengjunyi@live.com>
2020-04-30 16:36:45 +0200

    Update: Migrating CI to GitHub Actions

---
+++ .github/workflows/update.repo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+update_repo(){
+    cd homebrew-taps
+    status=`git status | grep "working tree clean" &> /dev/null; echo "$?"`
+    if [ "$status" == "1" ]; then
+        git commit -am "${formula_name} ${v_version}" || exit 0
+        git push  --quiet "https://${DEPLOY_TOKEN}@${GH_REF}" master:master
+    fi
+    cd ..
+}
+
+
+[ "$0" = "update.repo.sh" ] && git clone https://${GH_REF}
+[ "$0" = "update.repo.sh" ] && update_repo
+
